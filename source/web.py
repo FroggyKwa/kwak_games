1c058a6ce60d2872e623de273a512191bd260b73
FroggyKwa <ilya.laguxa@gmail.com>
2020-01-15 22:56:51 +1000

    pep8

--- source/web.py
+++ source/web.py
@@ -2,16 +2,14 @@ import socket
 
 
 def connect_InSocket(address='', port=5555):
-    UDP_PORT = port
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((address, UDP_PORT))
+    sock.bind((address, port))
     return sock
 
 
 def connect_OutSocket(address='localhost', port=5555):
-    UDP_PORT = port
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.connect(('localhost', UDP_PORT))
+    sock.connect((address, port))
     return sock
 
 
