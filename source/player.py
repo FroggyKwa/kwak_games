46dadcfbdf5f3cf2c5d77661278fbbeb1dca06f3
FroggyKwa <ilya.laguxa@gmail.com>
2020-01-16 20:43:24 +1000

    send to server

--- source/player.py
+++ source/player.py
@@ -1,4 +1,8 @@
+import json
+import socket
+
 import pygame
+import uuid
 
 
 JUMP_POWER = 10
@@ -7,13 +11,15 @@ MOVE_SPEED = 7
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x0, y0):
+    def __init__(self, x0, y0, addr, udp_port):
         super().__init__()
         self.image = pygame.Surface([20, 20])
         self.image.fill(pygame.color.Color('Red'))
         self.rect = self.image.get_rect()
         self.x_velocity = 0
         self.y_velocity = 0
+        self.id = str(uuid.uuid4())
+        self.udp_addr = (addr[0], int(udp_port))
         self.hp = 100
         self.onGround = False
         self.update(x0, y0)
@@ -41,3 +47,7 @@ class Player(pygame.sprite.Sprite):
 
     def get_damage(self, dmg):
         self.hp -= dmg if self.hp >= dmg else self.hp
+
+    def send_udp(self, player_identifier, message):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(json.dumps({player_identifier: message}), self.udp_addr)
